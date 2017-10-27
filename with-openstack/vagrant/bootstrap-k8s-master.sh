e9fa002a3cce7e322113d5a97c8b4a030bdefc09
Jim Lin <jimlintw922@gmail.com>
2017-10-27 11:59:15 +0800

    update bootstrap scripts

---
+++ with-openstack/vagrant/bootstrap-k8s-master.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+set -e
+
+ENV_MGMT_NETWORK="10.0.0.0/24"
+ENV_MGMT_OS_CONTROLLER_IP="10.0.0.11"
+ENV_MGMT_OS_NETWORK_IP="10.0.0.21"
+ENV_MGMT_OS_COMPUTE_IP="10.0.0.31"
+ENV_MGMT_ODL_CONTROLLER_IP="10.0.0.41"
+
+ENV_TUNNEL_NETWORK="10.0.1.0/24"
+ENV_TUNNEL_OS_CONTROLLER_IP="10.0.1.11"
+ENV_TUNNEL_OS_NETWORK_IP="10.0.1.21"
+ENV_TUNNEL_OS_COMPUTE_IP="10.0.1.31"
+ENV_TUNNEL_ODL_CONTROLLER_IP="10.0.1.41"
+
+LOG=/tmp/provision.log
+date | tee $LOG            # when:  Thu Aug 10 07:48:13 UTC 2017
+whoami | tee -a $LOG       # who:   root
+pwd | tee -a $LOG          # where: /home/vagrant
+
+CACHE=/vagrant/cache
+[ -d $CACHE ] || mkdir -p $CACHE 
+
+function use_public_apt_server() {
+    apt install -y software-properties-common
+    add-apt-repository cloud-archive:newton
+    apt-get update && APT_UPDATED=true
+
+    # Reference https://docs.openstack.org/newton/install-guide-ubuntu/environment-packages.html
+}
+
+function use_local_apt_server() {
+    cat > /etc/apt/sources.list <<DATA
+deb http://192.168.240.3/ubuntu xenial main restricted
+deb http://192.168.240.3/ubuntu xenial universe
+deb http://192.168.240.3/ubuntu xenial multiverse
+deb http://192.168.240.3/ubuntu xenial-updates main restricted
+deb http://192.168.240.3/ubuntu xenial-updates universe
+deb http://192.168.240.3/ubuntu xenial-updates multiverse
+deb http://192.168.240.3/ubuntu xenial-security main restricted
+deb http://192.168.240.3/ubuntu xenial-security universe
+deb http://192.168.240.3/ubuntu xenial-security multiverse
+deb http://192.168.240.3/ubuntu-cloud-archive xenial-updates/newton main
+DATA
+}
+
+function each_node_must_resolve_the_other_nodes_by_name_in_addition_to_IP_address() {
+    cat >> /etc/hosts <<DATA
+$ENV_MGMT_OS_CONTROLLER_IP os-controller
+$ENV_MGMT_OS_NETWORK_IP os-network
+$ENV_MGMT_OS_COMPUTE_IP os-compute
+$ENV_MGMT_ODL_CONTROLLER_IP odl-controller
+DATA
+
+    # Reference https://docs.openstack.org/newton/install-guide-ubuntu/environment-networking.html
+}
+
+function install_ntp() {
+    CHRONY_VERSION=2.1.1-1
+    [ "$APT_UPDATED" == "true" ] || apt-get update && APT_UPDATED=true
+    apt-get install -y chrony=$CHRONY_VERSION
+
+    # # # # # # # # # # # # # # # # ## # # # # # # # # # # # # # # # # # # # # # # # # ## # # # # # # # #
+
+    # To connect to the os-controller node
+    sed -i "s/^pool /#pool /g" /etc/chrony/chrony.conf
+    sed -i "s/^server /#server /g" /etc/chrony/chrony.conf
+    echo "server os-controller iburst" >> /etc/chrony/chrony.conf
+
+    # Restart the NTP service
+    service chrony restart
+
+    # Verify operation
+    chronyc sources
+
+    # Log files
+    # /var/log/chrony/measurements.log
+    # /var/log/chrony/statistics.log
+    # /var/log/chrony/tracking.log
+
+    # Reference https://docs.openstack.org/newton/install-guide-ubuntu/environment-ntp-other.html
+}
+
+function install_jdk() {
+    JDK_VERSION=8
+    [ "$APT_UPDATED" == "true" ] || apt-get update && APT_UPDATED=true
+    apt-get install -y openjdk-$JDK_VERSION-jdk
+}
+
+function download_odl() {
+    ODL_VERSION=0.5.4-Boron-SR4
+    [ -f $CACHE/distribution-karaf-$ODL_VERSION.tar.gz ] || \
+    wget -q https://nexus.opendaylight.org/content/repositories/public/org/opendaylight/integration/distribution-karaf/$ODL_VERSION/distribution-karaf-$ODL_VERSION.tar.gz -O $CACHE/distribution-karaf-$ODL_VERSION.tar.gz
+
+    tar -zxf $CACHE/distribution-karaf-$ODL_VERSION.tar.gz -C /opt
+    ln -sf /opt/distribution-karaf-$ODL_VERSION /opt/odl
+}
+
+function configure_odl() {
+    : 
+}
+
+function main() {
+    while [ $# -gt 0 ];
+    do
+        case $1 in
+            download)
+                #use_local_apt_server
+                use_public_apt_server
+                each_node_must_resolve_the_other_nodes_by_name_in_addition_to_IP_address
+                install_ntp
+                ;;
+            configure)
+                configure_odl
+                ;;
+            *)
+                echo "unknown mode"
+                ;;
+        esac
+        shift
+    done
+    echo done
+}
+main $@
