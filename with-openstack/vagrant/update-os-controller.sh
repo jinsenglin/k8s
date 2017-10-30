4d935124dbbe0e60f1192af89ab6af7d712f40e8
Jim Lin <jimlintw922@gmail.com>
2017-10-30 17:02:36 +0800

    update update-os-controller.sh

--- with-openstack/vagrant/update-os-controller.sh
+++ with-openstack/vagrant/update-os-controller.sh
@@ -112,7 +112,7 @@ DATA
 
 # # 4. 安裝 docker-engine 1.12
 apt-get install --no-install-recommends apt-transport-https curl software-properties-common
-curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add -
+curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | apt-key add -
 add-apt-repository "deb https://packages.docker.com/1.12/apt/repo/ ubuntu-$(lsb_release -cs) main"
 apt-get update
 apt-cache madison docker-engine
