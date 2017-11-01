8a796431adc9d73dc6aa0297e4ddd28920ce07c2
Jim Lin <jimlintw922@gmail.com>
2017-11-01 18:17:01 +0800

    update ha-master

--- ha-master/bring-up-5-machines.sh
+++ ha-master/bring-up-5-machines.sh
@@ -21,66 +21,81 @@ PN=infra
 PSN=infra
 
 # private IPs
+PIP0=192.168.202.100
 PIP1=192.168.202.101
 PIP2=192.168.202.102
 PIP3=192.168.202.103
 PIP4=192.168.202.104
 PIP5=192.168.202.105
-PIP6=192.168.202.106
 
-# Reserve resources # TODO Delete
+# Reserve resources
 function reserve_PIPs() {
-    PIP1_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP1 -f value -c id $PIP1)
+    PIP0_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP0 -f value -c id $PIP0)
+    echo "export PIP0_PORT_ID=$PIP0_PORT_ID" | tee -a rc
+
+    PIP1_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP1 --allowed-address ip-address=$PIP0 -f value -c id $PIP1)
     echo "export PIP1_PORT_ID=$PIP1_PORT_ID" | tee -a rc
-    PIP2_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP2 -f value -c id $PIP2)
+
+    PIP2_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP2 --allowed-address ip-address=$PIP0 -f value -c id $PIP2)
     echo "export PIP2_PORT_ID=$PIP2_PORT_ID" | tee -a rc
-    PIP3_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP3 -f value -c id $PIP3)
+
+    PIP3_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP3 --allowed-address ip-address=$PIP0 -f value -c id $PIP3)
     echo "export PIP3_PORT_ID=$PIP3_PORT_ID" | tee -a rc
-    PIP4_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP4 -f value -c id $PIP4)
+
+    PIP4_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP4 --allowed-address ip-address=$PIP0 -f value -c id $PIP4)
     echo "export PIP4_PORT_ID=$PIP4_PORT_ID" | tee -a rc
-    PIP5_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP5 -f value -c id $PIP5)
+
+    PIP5_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP5 --allowed-address ip-address=$PIP0 -f value -c id $PIP5)
     echo "export PIP5_PORT_ID=$PIP5_PORT_ID" | tee -a rc
-    PIP6_PORT_ID=$(openstack port create --network $PN --fixed-ip subnet=$PSN,ip-address=$PIP6 -f value -c id $PIP6)
-    echo "export PIP6_PORT_ID=$PIP6_PORT_ID" | tee -a rc
 }
 #reserve_PIPs
 
 # external network name
 EN=Catherine-924
 
-# Reserve resources # TODO Delete
+# Reserve resources
 function reserve_FIPs() {
     source rc
+    FIP0_ID=$(openstack floating ip create --port $PIP0_PORT_ID --fixed-ip-address $PIP0 -f value -c id $EN)
+    echo "export FIP0_ID=$FIP0_ID" | tee -a rc
+
     FIP1_ID=$(openstack floating ip create --port $PIP1_PORT_ID --fixed-ip-address $PIP1 -f value -c id $EN)
     echo "export FIP1_ID=$FIP1_ID" | tee -a rc
+
     FIP2_ID=$(openstack floating ip create --port $PIP2_PORT_ID --fixed-ip-address $PIP2 -f value -c id $EN)
     echo "export FIP2_ID=$FIP2_ID" | tee -a rc
+
     FIP3_ID=$(openstack floating ip create --port $PIP3_PORT_ID --fixed-ip-address $PIP3 -f value -c id $EN)
     echo "export FIP3_ID=$FIP3_ID" | tee -a rc
+
     FIP4_ID=$(openstack floating ip create --port $PIP4_PORT_ID --fixed-ip-address $PIP4 -f value -c id $EN)
     echo "export FIP4_ID=$FIP4_ID" | tee -a rc
+
     FIP5_ID=$(openstack floating ip create --port $PIP5_PORT_ID --fixed-ip-address $PIP5 -f value -c id $EN)
     echo "export FIP5_ID=$FIP5_ID" | tee -a rc
-    FIP6_ID=$(openstack floating ip create --port $PIP6_PORT_ID --fixed-ip-address $PIP6 -f value -c id $EN)
-    echo "export FIP6_ID=$FIP6_ID" | tee -a rc
 }
 #reserve_FIPs
 
 # Export floating IPs
 function export_FIPs() {
     source rc
+    FIP0=$(openstack floating ip show $FIP0_ID -f value -c floating_ip_address)
+    echo "export FIP0=$FIP0" | tee -a rc
+
     FIP1=$(openstack floating ip show $FIP1_ID -f value -c floating_ip_address)
     echo "export FIP1=$FIP1" | tee -a rc
+
     FIP2=$(openstack floating ip show $FIP2_ID -f value -c floating_ip_address)
     echo "export FIP2=$FIP2" | tee -a rc
+
     FIP3=$(openstack floating ip show $FIP3_ID -f value -c floating_ip_address)
     echo "export FIP3=$FIP3" | tee -a rc
+
     FIP4=$(openstack floating ip show $FIP4_ID -f value -c floating_ip_address)
     echo "export FIP4=$FIP4" | tee -a rc
+
     FIP5=$(openstack floating ip show $FIP5_ID -f value -c floating_ip_address)
     echo "export FIP5=$FIP5" | tee -a rc
-    FIP6=$(openstack floating ip show $FIP6_ID -f value -c floating_ip_address)
-    echo "export FIP6=$FIP6" | tee -a rc
 }
 #export_FIPs
 
