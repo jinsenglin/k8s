b44db2bf4642ccdfb8790a3ad2cf980f9e1153f1
Jim Lin <jimlintw922@gmail.com>
2017-11-01 09:44:21 +0800

    coding run-e2e-tests.sh

--- with-openstack/vagrant/run-e2e-tests.sh
+++ with-openstack/vagrant/run-e2e-tests.sh
@@ -2,19 +2,49 @@
 
 set -e
 
-CACHE=/vagrant/cache
+source /root/admin-openrc
+source /vagrant/cache/env.rc
+
+# check
+#nova-status upgrade check # RUN ON OS-CONTROLLER
+openstack network agent list
+openstack image list
+openstack catalog list
+openstack hypervisor list
+openstack role assignment list --user kuryr --project service --user-domain Default --project-domain Default
+openstack service list
+
+openstack subnet pool list
+openstack subnet pool show shared-default-subnetpool
+
+openstack project list
+openstack project show demo
+
+openstack network list
+openstack network show demo
+
+openstack subnet list
+openstack subnet show demo
+openstack subnet show k8s-service-subnet
+
+openstack router list
+openstack router show demo
+
+openstack port list
+
+# --------------------------------------------------------------------------------------------
 
 source /root/demo-openrc
-source $CACHE/env.rc
+source /vagrant/cache/env.rc
 
 # create openstack server 'testvm'
 #nova boot --flavor m1.nano --image cirros --nic net-id=$DEMO_NET_ID --config-drive=true testvm
 
-# create k8s deployment 'demo'
+# create k8s deployment 'demo' # FAILED
 #kubectl run demo --image=demo:demo
 
-K8S_POD1_NAME=$(kubectl get pods -l run=demo -o jsonpath='{.items[].metadata.name}')
-echo "K8S_POD1_NAME = $K8S_POD1_NAME"
+#K8S_POD1_NAME=$(kubectl get pods -l run=demo -o jsonpath='{.items[].metadata.name}')
+#echo "K8S_POD1_NAME = $K8S_POD1_NAME"
 
 # create k8s service 'demo'
 #kubectl expose deployment demo --port=80 --target-port=8080
@@ -26,7 +56,7 @@ echo "K8S_POD1_NAME = $K8S_POD1_NAME"
 kubectl get services demo
 kubectl get endpoints demo
 
-kubectl exec -it $DEMO_POD_NAME -- bash
+#kubectl exec -it $DEMO_POD_NAME -- bash
 
 <<SOP
 export PS1='POD # '
