ffa4818271f15adeefacf228584dcd699452b1b1
Jim Lin <jimlintw922@gmail.com>
2017-11-02 13:17:52 +0800

    update config script

--- ha-master/configure-machine.sh
+++ ha-master/configure-machine.sh
@@ -25,7 +25,36 @@ function bring_up_etcd_cluster() {
     case $HOSTNAME in
         $M1)
             echo "M1"
-            docker ps --filter "name=etcd" | wc -l
+
+            flag=$(docker ps --filter "name=etcd" | wc -l)
+            if [ $flag -gt 1 ]; then
+                docker stop etcd
+                docker rm etcd
+            fi
+
+            [ -d /var/lib/etcd-cluster ] && rm -rf /var/lib/etcd-cluster
+            mkdir -p /var/lib/etcd-cluster
+
+            docker run -d \
+            --restart always \
+            -v /etc/ssl/certs:/etc/ssl/certs \
+            -v /var/lib/etcd-cluster:/var/lib/etcd \
+            -p 4001:4001 \
+            -p 2380:2380 \
+            -p 2379:2379 \
+            --name etcd \
+            gcr.io/google_containers/etcd-amd64:3.0.17 \
+            etcd --name=etcd0 \
+            --advertise-client-urls=http://$PIP1:2379,http://$PIP1:4001 \
+            --listen-client-urls=http://0.0.0.0:2379,http://0.0.0.0:4001 \
+            --initial-advertise-peer-urls=http://$PIP1:2380 \
+            --listen-peer-urls=http://0.0.0.0:2380 \
+            --initial-cluster-token=9477af68bbee1b9ae037d6fd9e7efefd \
+            --initial-cluster=etcd0=http://$PIP1:2380,etcd1=http://$PIP2:2380,etcd2=http://$PIP3:2380 \
+            --initial-cluster-state=new \
+            --auto-tls \
+            --peer-auto-tls \
+            --data-dir=/var/lib/etcd
             ;;
         $M2)
             echo "M2"
