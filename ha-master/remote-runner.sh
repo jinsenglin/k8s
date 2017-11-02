f4cb820dd002fd3314aa568f44453febd9ee2bc8
Jim Lin <jimlintw922@gmail.com>
2017-11-02 16:20:49 +0800

    update remote-runner.sh

--- ha-master/remote-runner.sh
+++ ha-master/remote-runner.sh
@@ -3,7 +3,8 @@
 # Usage 1: bash remote-runner.sh $FIP1 ls /
 # Usage 2: bash remote-runner.sh $FIP1 "cd k8s; git pull"
 # Usage 3: bash remote-runner.sh $FIP1 "cd k8s/ha-master; bash copy-packages-to-machine.sh"
-# Usage 3: bash remote-runner.sh $FIP1 "cd k8s/ha-master; bash configure-machine.sh"
+# Usage 4: bash remote-runner.sh $FIP1 "cd k8s/ha-master; bash configure-machine.sh"
+# Usage 5: bash remote-runner.sh $FIP1 "cd k8s/ha-master; bash destroy-k8s-cluster.sh"
 
 IP=$1
 shift
