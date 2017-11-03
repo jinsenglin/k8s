6c5364db3fd8682a98af419485c3b5f7c8893607
Jim Lin <jimlintw922@gmail.com>
2017-11-03 16:23:55 +0800

    update config script

--- ha-master/easy-configure.sh
+++ ha-master/easy-configure.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-git add -A
-git commit -m "update config script"
-git push
+has_changes=$(git status | grep "Changes not staged for commit:" | wc -l)
+has_untracked=$(git status | grep "Untracked files:" | wc -l)
+
+if [ $has_changes == 1 -o $has_untracked == 1 ]; then
+    git add -A
+    git commit -m "update config script"
+    git push
+fi
 
 bash remote-runner-wrapper.sh git_pull
 bash remote-runner-wrapper.sh configure
