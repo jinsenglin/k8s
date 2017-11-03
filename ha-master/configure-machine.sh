237c9af50a6160b9092a645ab59c1c3fce8a42e9
Jim Lin <jimlintw922@gmail.com>
2017-11-03 17:59:21 +0800

    update config script

--- ha-master/configure-machine.sh
+++ ha-master/configure-machine.sh
@@ -701,8 +701,8 @@ function main() {
     #            echo "wait 10 seconds for k8s pods up and running"
     #            i=10; while [ $i -gt 0 ]; do echo "wait for $i seconds"; i=$(( $i - 1 )); sleep 1; done
     
-    check_k8s_cluster_ha    # 5:58
-    #scale_kube_dns
+    #check_k8s_cluster_ha    # 5:58
+    scale_kube_dns          # 5:59
     #check_k8s_cluster_ha
     #setup_keepalived   # TODO replace hard-coded IP in config files
     #setup_nginx_lb     # TODO replace hard-coded IP in config files
