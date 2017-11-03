ca497e27a05db72d692c1d7dc3f6837a2f65a3c9
Jim Lin <jimlintw922@gmail.com>
2017-11-03 17:52:45 +0800

    update config script

--- ha-master/configure-machine.sh
+++ ha-master/configure-machine.sh
@@ -692,8 +692,8 @@ function main() {
     #            echo "wait 10 seconds for k8s pods up and running"
     #            i=10; while [ $i -gt 0 ]; do echo "wait for $i seconds"; i=$(( $i - 1 )); sleep 1; done
     
-    check_k8s_cluster       # 5:51
-    #install_flannel
+    #check_k8s_cluster       # 5:51
+    install_flannel         # 5:52
     #update_kube_apiserver
     #check_k8s_cluster
     #setup_ha_master
