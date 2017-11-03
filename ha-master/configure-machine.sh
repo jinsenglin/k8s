81dd7bfd13dc4e1e9157cdea50874e00a6e368f8
Jim Lin <jimlintw922@gmail.com>
2017-11-03 13:59:29 +0800

    update config script

--- ha-master/configure-machine.sh
+++ ha-master/configure-machine.sh
@@ -660,7 +660,8 @@ function main() {
     #            echo "wait 10 seconds for k8s pods up and running"
     #            i=10; while [ $i -gt 0 ]; do echo "wait for $i seconds"; i=$(( $i - 1 )); sleep 1; done
 
-    check_kube_proxy
+    #check_kube_proxy
+    check_k8s_cluster_ha
     #run_kubeadm_join
 }
 
