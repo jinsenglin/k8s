612202d9c67e1df4758ddb84179be98d7b1d481d
Jim Lin <jimlintw922@gmail.com>
2017-11-06 17:19:38 +0800

    update easy-kubectl.sh

--- ha-master/easy-kubectl.sh
+++ ha-master/easy-kubectl.sh
@@ -11,6 +11,22 @@ function case_run_nginx() {
     bash remote-runner.sh $FIPC kubectl run hello-nginx --image=nginx:latest --replicas=1 --port=80
 }
 
+function case_scale_nginx() {
+    source rc
+    bash remote-runner.sh $FIPC kubectl scale deployment/hello-nginx --replicas=3
+}
+
+function case_expose_nginx() {
+    source rc
+    bash remote-runner.sh $FIPC kubectl expose deployment/hello-nginx --target-port=80
+}
+
+function case_del_nginx() {
+    source rc
+    bash remote-runner.sh $FIPC kubectl delete deployment/hello-nginx
+    bash remote-runner.sh $FIPC kubectl delete service/hello-nginx
+}
+
 CMD=$1
 
 case $CMD in
