868ead3c9f0104fe568ff5fc3d97f0b80f75c9a6
Jim Lin <jimlintw922@gmail.com>
2017-11-06 17:03:55 +0800

    update easy-kubectl.sh

--- ha-master/easy-kubectl.sh
+++ ha-master/easy-kubectl.sh
@@ -2,17 +2,5 @@
 
 set -e
 
-CMD=$1
-shift
-
-function get_node() {
-    source rc
-    bash remote-runner.sh $FIPC kubectl get node
-}
-
-function get_po() {
-    source rc
-    bash remote-runner.sh $FIPC kubectl get po
-}
-
-$CMD $@
+source rc
+bash remote-runner.sh $FIPC kubectl $@
