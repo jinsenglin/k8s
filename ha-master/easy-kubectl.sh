adeb49fabac4febc863213d93b44ca622846aef5
Jim Lin <jimlintw922@gmail.com>
2017-11-06 17:10:12 +0800

    update easy-kubectl.sh

--- ha-master/easy-kubectl.sh
+++ ha-master/easy-kubectl.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+# Usage: bash easy-kubectl.sh get node -o wide
+# Usage: bash easy-kubectl.sh get po -n kube-system -o wide
+# Usage: bash easy-kubectl.sh case run_nginx
+
 set -e
 
-source rc
-bash remote-runner.sh $FIPC kubectl $@
+function case_run_nginx() {
+    source rc
+    bash remote-runner.sh $FIPC kubectl run hello-nginx --image=nginx:latest --replicas=1 --port=80
+}
+
+CMD=$1
+
+case $CMD in
+    "case")
+        shift
+        CASE=$1
+        case_$CASE
+        ;;
+    *)
+        source rc
+        bash remote-runner.sh $FIPC kubectl $@
+        ;;
+esac
