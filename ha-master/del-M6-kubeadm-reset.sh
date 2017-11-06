da1366a734d3a886372424af5621e894850d6f30
Jim Lin <jimlintw922@gmail.com>
2017-11-06 18:20:33 +0800

    update del-M6-kubeadm-reset.sh

--- ha-master/del-M6-kubeadm-reset.sh
+++ ha-master/del-M6-kubeadm-reset.sh
@@ -5,7 +5,7 @@ set -e
 function configure() {
     source rc
 
-    bash easy-configure.sh delete node $M6
+    bash easy-kubectl.sh delete node $M6
 
     set +e
     bash remote-runner.sh $FIP6 "kubeadm reset; [ -d /var/lib/kubelet ] && rm -rf /var/lib/kubelet; [ -f /etc/kubernetes/bootstrap-kubelet.conf ] && rm /etc/kubernetes/bootstrap-kubelet.conf"
