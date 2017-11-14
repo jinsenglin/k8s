c38221d5b1abd6f6e170ce08ac09af7a2966d724
Jim Lin <jimlintw922@gmail.com>
2017-11-14 18:19:56 +0800

    update config script

--- ha-master/configure-machine.sh
+++ ha-master/configure-machine.sh
@@ -805,8 +805,8 @@ function main() {
     #check_kube_proxy        # 6:04
     #check_k8s_cluster_ha    # 6:05
     #make_masters_unscheduled
-    add_node                # 6:05
-    #check_k8s_cluster_ha    # 6:11
+    #add_node                # 6:05
+    check_k8s_cluster_ha    # 6:11
     #install_dashboard
     #install_heapster
 }
