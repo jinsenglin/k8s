e9e9a0a0e4db44de5d1f0bd48d0dc09a8ab3fabb
Jim Lin <jimlintw922@gmail.com>
2018-05-25 15:06:56 +0800

    mod 03-k8s-prom-hpa

--- workload/03-k8s-prom-hpa/install.sh
+++ workload/03-k8s-prom-hpa/install.sh
@@ -8,4 +8,24 @@ kubectl apply -f k8s-prom-hpa/metrics-server
 
 kubectl apply -f k8s-prom-hpa/namespaces.yaml
 kubectl apply -f k8s-prom-hpa/prometheus
+
+<<OPT1
+yum install golang
+    wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+    chmod +x cfssl_linux-amd64
+    mv cfssl_linux-amd64 /usr/local/bin/cfssl
+
+    wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+    chmod +x cfssljson_linux-amd64
+    mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
+
+    export PATH=$PATH:/usr/local/bin
+make certs
+
+cd k8s-prom-hpa
+kubectl apply -f ./custom-metrics-api
+OPT1
+
+# OPT2
+cp cm-adapter-serving-certs.yaml k8s-prom-hpa/custom-metrics-api
 kubectl apply -f k8s-prom-hpa/custom-metrics-api
