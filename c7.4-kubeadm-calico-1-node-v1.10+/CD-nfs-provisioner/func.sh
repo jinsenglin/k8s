fba9fad7b890f007c876e3e726de7266ab2c6302
cclin <jimlintw922@gmail.com>
2018-07-20 20:37:50 +0800

    mod CD-nfs-provisioner/func.sh

--- c7.4-kubeadm-calico-1-node-v1.10+/CD-nfs-provisioner/func.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/CD-nfs-provisioner/func.sh
@@ -1,7 +1,7 @@
 RELEASE_NAME=nfs-provisioner-release
 
 function create_nfs_provisioner() {
-    helm install stable/nfs-server-provisioner --namespace add-on --name $RELEASE_NAME
+    helm install stable/nfs-server-provisioner --namespace add-on --name $RELEASE_NAME --set storageClass.defaultClass=true
 }
 
 function delete_nfs_provisioner() {
