8af656343370848e6175d5fa2f0e03ac9a8e3c2e
cclin <jimlintw922@gmail.com>
2018-07-21 22:11:42 +0800

    mod 02-try-run.sh

--- c7.4-kubeadm-calico-1-node-v1.10+/controller-development-quickstart/02-try-run.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/controller-development-quickstart/02-try-run.sh
@@ -4,14 +4,18 @@ set -e
 set -o pipefail
 
 function main() {
-    # just copy and paste the sample code
-    # then 
-    dep ensure
-    go build -o out/pod-monitor github.com/cclin81922/k8s-pod-monitor/cmd/pod-monitor
-    make
-    docker build -t k8s-pod-monitor:latest .
-    kubectl apply -f k8s-pod.yaml
-    kubectl delete -f k8s-pod.yaml
+    export GOPATH=~/go
+    export KUBECONFIG=/etc/kubernetes/admin.conf
+
+    cd $GOPATH/src/github.com/cclin81922/k8s-pod-monitor
+        # just copy and paste the sample code
+        # then 
+        dep ensure
+        go build -o out/pod-monitor github.com/cclin81922/k8s-pod-monitor/cmd/pod-monitor
+        make
+        docker build -t k8s-pod-monitor:latest .
+        kubectl apply -f k8s-pod.yaml
+        kubectl delete -f k8s-pod.yaml
 }
 
 main
