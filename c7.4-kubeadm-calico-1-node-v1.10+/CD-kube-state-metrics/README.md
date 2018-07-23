7aea5e05546d841001802d83dc4cc79e8384f12c
Jim Lin <jimlintw922@gmail.com>
2018-07-23 17:20:34 +0800

    expand CD-kube-state-metrics

---
+++ c7.4-kubeadm-calico-1-node-v1.10+/CD-kube-state-metrics/README.md
@@ -0,0 +1,5 @@
+# verify
+
+```
+curl http://kube-state-metrics.add-on.k8s.local/metrics
+```
--- c7.4-kubeadm-calico-1-node-v1.10+/CD-kube-state-metrics/func.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/CD-kube-state-metrics/func.sh
@@ -2,8 +2,10 @@ RELEASE_NAME=kube-state-metrics-release
 
 function create_kube_state_metrics() {
     helm install stable/kube-state-metrics --namespace add-on --name $RELEASE_NAME --set rbac.create=true
+    kubectl apply -f Ingress.yaml 
 }
 
 function delete_kube_state_metrics() {
+    kubectl delete -f Ingress.yaml
     helm delete --purge $RELEASE_NAME
 }
