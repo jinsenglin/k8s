b7124489e19ad15d4d694d98269b11b875aa8e72
Jim Lin <jimlintw922@gmail.com>
2018-07-23 09:39:05 +0800

    mod 00-prepare.sh

--- c7.4-kubeadm-calico-1-node-v1.10+/00-prepare.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/00-prepare.sh
@@ -39,6 +39,7 @@ function nofirewall() {
 
 function enable_bridge_nf_call_iptables() {
     cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
