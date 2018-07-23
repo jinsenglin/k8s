5ab9c5086d5599dce590b2b157f9fd4d5ec51cce
Jim Lin <jimlintw922@gmail.com>
2018-07-23 16:48:01 +0800

    mod sample-code/skaffold.yaml

--- c7.4-kubeadm-calico-1-node-v1.10+/controller-development-quickstart/03-try-skaffold.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/controller-development-quickstart/03-try-skaffold.sh
@@ -12,6 +12,10 @@ function main() {
         # cp sample-code/skaffold.yaml .
         # then
         skaffold run
+        kubectl -n dev get po
+        kubectl -n dev logs pod-monitor
+        kubectl delete -f k8s-namespace.yaml -f k8s-clusterrolebinding.yaml -f k8s-pod.yaml
+        # need manually remove all the auto-generated images
         git add -A
         git commit -m "pass try-skaffold"
 }
