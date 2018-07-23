fcd33b6dd58f7a0e2c67038c0ee236fc0eba62d2
Jim Lin <jimlintw922@gmail.com>
2018-07-23 13:18:09 +0800

    mod CD-fluent-bit

--- c7.4-kubeadm-calico-1-node-v1.10+/CD-fluent-bit/func.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/CD-fluent-bit/func.sh
@@ -1,9 +1,11 @@
 RELEASE_NAME=fluent-bit-release
 
 function create_nginx_ingress() {
-    helm install stable/fluent-bit --namespace add-on --name $RELEASE_NAME --set backend.type=es --set backend.es.host=10.112.0.3
+    kubectl apply -f Service.yaml
+    helm install stable/fluent-bit --namespace add-on --name $RELEASE_NAME --set backend.type=es --set backend.es.host=ext-es
 }
 
 function delete_nginx_ingress() {
     helm delete --purge $RELEASE_NAME
+    kubectl delete -f Service.yaml
 }
