85867310f3e5914e217e378aaee2f544153f93c2
Jim Lin <jimlintw922@gmail.com>
2018-07-24 10:13:42 +0000

    mod CD-kubernetes-dashboard-

--- c7.4-kubeadm-calico-1-node-v1.10+/CD-kubernetes-dashboard-/func.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/CD-kubernetes-dashboard-/func.sh
@@ -1,9 +1,11 @@
 RELEASE_NAME=kubernetes-dashboard-release
 
 function create_nginx_ingress() {
-    helm install stable/kubernetes-dashboard --namespace add-on --name $RELEASE_NAME --set ingress.enabled=true --set rbac.clusterAdminRole=true
+    helm install stable/kubernetes-dashboard --namespace add-on --name $RELEASE_NAME --set rbac.clusterAdminRole=true
+    kubectl apply -f Ingress.yaml 
 }
 
 function delete_nginx_ingress() {
+    kubectl delete -f Ingress.yaml
     helm delete --purge $RELEASE_NAME
 }
