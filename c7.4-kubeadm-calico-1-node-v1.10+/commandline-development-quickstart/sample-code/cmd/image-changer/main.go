12120f4322bcb8a87d819119e2a76b3caa0975c8
Jim Lin <jimlintw922@gmail.com>
2018-07-25 05:10:24 +0000

    done sample-code

--- c7.4-kubeadm-calico-1-node-v1.10+/commandline-development-quickstart/sample-code/cmd/image-changer/main.go
+++ c7.4-kubeadm-calico-1-node-v1.10+/commandline-development-quickstart/sample-code/cmd/image-changer/main.go
@@ -4,12 +4,12 @@ import (
     "log"
 
     "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/apimachinery/pkg/api/errors"
 )
 
 func main() {
     // parses the flag
-    //kubeconfig, namespaceName, deploymentName, containerName, imageName := ParseFlag()
-    kubeconfig, namespaceName, deploymentName, _, _ := ParseFlag()
+    kubeconfig, namespaceName, deploymentName, containerName, imageName := ParseFlag()
 
     // creates the config
     config := GetKubernetesConfig(kubeconfig)
@@ -17,10 +17,38 @@ func main() {
     // creates the client
     client := GetKubernetesClient(config)
 
-    // changes the image
-    //deployment, err := client.AppsV1beta1().Deployments(namespaceName).Get(deploymentName, v1.GetOptions{})
-    _, err := client.AppsV1beta1().Deployments(namespaceName).Get(deploymentName, v1.GetOptions{})
-    if err != nil {
-        log.Panic(err.Error())
+    // get the deployment
+    deployment, err := client.AppsV1beta1().Deployments(namespaceName).Get(deploymentName, v1.GetOptions{})
+    if errors.IsNotFound(err) {
+        log.Panicf("Deployment %s in namespace %s not found\n", deploymentName, namespaceName)
+    } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+        log.Panicf("Error getting deployment %s in namespace %s: %v\n", deploymentName, namespaceName, statusError.ErrStatus.Message)
+    } else if err != nil {
+        panic(err.Error())
+    } else {
+        log.Printf("Found deployment %s in namespace %s\n", deploymentName, namespaceName)
+
+        // get the container
+        containers := &deployment.Spec.Template.Spec.Containers
+        found := false
+        for i := range *containers {
+            c := *containers
+            if c[i].Name == containerName {
+                found = true
+
+                // update the image
+                c[i].Image = imageName
+            }
+        }
+        if found == false {
+            log.Panicf("Container %s in deployment %s not found\n", containerName, deploymentName)
+        }
+
+        // apply the update
+        _, err := client.AppsV1beta1().Deployments(namespaceName).Update(deployment)
+        if err != nil {
+            panic(err.Error())
+        }
     }
+   
 }
