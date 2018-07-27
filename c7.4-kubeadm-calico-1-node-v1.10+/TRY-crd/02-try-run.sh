d6de58eea187042fb9c4c634be480d81b35809e6
Jim Lin <jimlintw922@gmail.com>
2018-07-27 03:22:37 +0000

    mod TRY-crd

--- c7.4-kubeadm-calico-1-node-v1.10+/TRY-crd/02-try-run.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/TRY-crd/02-try-run.sh
@@ -8,12 +8,18 @@ function main() {
     export KUBECONFIG=/etc/kubernetes/admin.conf
 
     cd $GOPATH/src/k8s.io/sample-controller
+        # Terminal 1
         kubectl create -f artifacts/examples/crd.yaml
-        kubectl create -f artifacts/examples/example-foo.yaml
         go run *.go -kubeconfig=$KUBECONFIG
-        kubectl get deployments
-        kubectl delete -f artifacts/examples/example-foo.yaml
         kubectl delete -f artifacts/examples/crd.yaml
+
+        # Terminal 2
+        kubectl create -f artifacts/examples/example-foo.yaml
+        kubectl get foo
+        kubectl get deployments example-foo
+        kubectl delete -f artifacts/examples/example-foo.yaml
+        kubectl get foo
+        kubectl get deployments example-foo
 }
 
 main
