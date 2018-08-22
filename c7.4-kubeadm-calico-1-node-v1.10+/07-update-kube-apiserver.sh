5c449ee121cf799ac72bb26ad36a2b4d1ae83257
Jim Lin <jimlintw922@gmail.com>
2018-08-22 06:24:19 +0000

    mod 07-update-kube-apiserver.sh

--- c7.4-kubeadm-calico-1-node-v1.10+/07-update-kube-apiserver.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/07-update-kube-apiserver.sh
@@ -15,10 +15,53 @@ function update_enable_resource_quota() {
 
 function update_enable_authz_webhook() {
     #
+    # MODIFY the file "/etc/kubernetes/manifests/kube-apiserver.yaml"
+    #
+    # AS-IS --authorization-mode=Node,RBAC
+    # TO-BE --authorization-mode=Node,RBAC,Webhook
+    #
+    # AS-IS
+    # TO-BE --authorization-webhook-config-file=/etc/kubernetes/webhook.kubeconfig
+    # 
+    # volumeMounts:
+    # ...
+    # - mountPath: /etc/kubernetes/webhook.kubeconfig
+    #   name: webhook-kubeconfig-file
+    #   readOnly: true
+    #
+    # volumes:
+    # ...
+    # - hostPath:
+    #     path: /etc/kubernetes/authn-authz-webhook/webhook.kubeconfig
+    #     type: File
+    #   name: webhook-kubeconfig-file
     :
 }
 
 function update_enable_authn_token_webhook() {
     #
+    # MODIFY the file "/etc/kubernetes/manifests/kube-apiserver.yaml"
+    #
+    # AS-IS
+    # TO-BE --runtime-config=authentication.k8s.io/v1beta1=true
+    #
+    # AS-IS
+    # TO-BE --authentication-token-webhook-config-file=/etc/kubernetes/webhook.kubeconfig
+    #
+    # AS-IS
+    # TO-BE --authentication-token-webhook-cache-ttl=5m
+    #
+    # volumeMounts:
+    # ...
+    # - mountPath: /etc/kubernetes/webhook.kubeconfig
+    #   name: webhook-kubeconfig-file
+    #   readOnly: true
+    #
+    # volumes:
+    # ...
+    # - hostPath:
+    #     path: /etc/kubernetes/authn-authz-webhook/webhook.kubeconfig
+    #     type: File
+    #   name: webhook-kubeconfig-kubeconfig
     :
 }
