3ced7ee6d4b2cd66ce58355932444ea3d01da6c3
Jim Lin <jimlintw922@gmail.com>
2018-08-23 17:37:23 +0800

    mod func.sh Deployment.yaml

--- c7.4-kubeadm-calico-1-node-v1.10+/CD-userX-namespace/with-keystone/func.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/CD-userX-namespace/with-keystone/func.sh
@@ -1,4 +1,5 @@
 function create_userX_namespace() {
+    kubectl create clusterrolebinding default-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
     kubectl create secret generic keystone-auth-certs --from-file=cert-file=/etc/kubernetes/pki/apiserver.crt --from-file=key-file=/etc/kubernetes/pki/apiserver.key -n kube-system
     kubectl apply -f .
 }
@@ -6,4 +7,5 @@ function create_userX_namespace() {
 function delete_userX_namespace() {
     kubectl delete -f .
     kubectl delete secret keystone-auth-certs -n kube-system
+    kubectl delete clusterrolebinding default-cluster-admin
 }
