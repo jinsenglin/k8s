76a8b7b8d7e988dd7174286c71ecac80a3cd4da8
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2018-08-23 18:35:48 +0800

    Update 05-check-v1.11.1.sh

--- c7.4-kubeadm-calico-1-node-v1.10+/05-check-v1.11.1.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/05-check-v1.11.1.sh
@@ -10,7 +10,7 @@ function check_helm() {
     # The tiller pod's status is pending because it doesn't tolerate the taints
     #
     # [ option 1: remove the taint ]
-    # kubectl taint nodes k8s.novalocal node-role.kubernetes.io/master-
+    # kubectl taint nodes k8s node-role.kubernetes.io/master-
     #
     # [ option 2: tolerate the taint ]
     #
@@ -46,11 +46,11 @@ function check_pod() {
 NAME                                    READY     STATUS    RESTARTS   AGE
 calico-node-zgfm4                       2/2       Running   0          1m
 etcd-k8s.novalocal                      1/1       Running   0          51s
-kube-apiserver-k8s.novalocal            1/1       Running   0          52s
-kube-controller-manager-k8s.novalocal   1/1       Running   0          48s
+kube-apiserver-k8s                      1/1       Running   0          52s
+kube-controller-manager-k8s             1/1       Running   0          48s
 kube-dns-86f4d74b45-hm25m               3/3       Running   0          1m
 kube-proxy-fxd7z                        1/1       Running   0          1m
-kube-scheduler-k8s.novalocal            1/1       Running   0          35s
+kube-scheduler-k8s                      1/1       Running   0          35s
 
 # b. coredns
 
@@ -58,11 +58,11 @@ NAME                                    READY     STATUS    RESTARTS   AGE
 calico-node-s47zx                       2/2       Running   0          1m
 coredns-78fcdf6894-4b6j6                1/1       Running   0          3m
 coredns-78fcdf6894-cwblh                1/1       Running   0          3m
-etcd-k8s.novalocal                      1/1       Running   0          3m
-kube-apiserver-k8s.novalocal            1/1       Running   0          2m
-kube-controller-manager-k8s.novalocal   1/1       Running   0          2m
+etcd-k8s                                1/1       Running   0          3m
+kube-apiserver-k8s                      1/1       Running   0          2m
+kube-controller-manager-k8s             1/1       Running   0          2m
 kube-proxy-6bh65                        1/1       Running   0          3m
-kube-scheduler-k8s.novalocal            1/1       Running   0          2m
+kube-scheduler-k8s                      1/1       Running   0          2m
 OUTPUT
 }
 
