cec80bce61d7989b88a4c753a58165c6f5f42e35
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2018-08-24 21:51:16 +0800

    Update 07-update-kubelet.sh

--- c7.4-kubeadm-calico-1-node-v1.10+/07-update-kubelet.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/07-update-kubelet.sh
@@ -10,6 +10,17 @@ function open_10255() {
     # AS-IS ExecStart=/usr/bin/kubelet $KUBELET_KUBECONFIG_ARGS $KUBELET_CONFIG_ARGS $KUBELET_KUBEADM_ARGS $KUBELET_EXTRA_ARGS
     # TO-BE ExecStart=/usr/bin/kubelet $KUBELET_KUBECONFIG_ARGS $KUBELET_CONFIG_ARGS $KUBELET_KUBEADM_ARGS $KUBELET_EXTRA_ARGS --read-only-port=10255
     #
+    
+    # USAGE
+    # http://localhost:10255/metrics/           # kubelet internal server
+    # http://localhost:10255/metrics/cadvisor   # kubelet internal server
+    # http://localhost:10255/pods/              # kubelet internal server
+    # http://localhost:10255/stats/             # kubelet internal server
+    # http://localhost:10255/stats/summary      # kubelet internal server
+    #
+    # ABOUT metrics/cadvisor
+    # * https://prometheus.io/docs/guides/cadvisor/
+    # * curl http://localhost:10255/metrics/cadvisor | grep container_memory_usage_bytes
 
     systemctl daemon-reload
     systemctl restart kubelet
