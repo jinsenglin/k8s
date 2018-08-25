7c004b86f434acac1882b38688e87413ba2d94ef
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2018-08-25 08:54:40 +0800

    Update 07-update-kubelet.sh

--- c7.4-kubeadm-calico-1-node-v1.10+/07-update-kubelet.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/07-update-kubelet.sh
@@ -15,6 +15,7 @@ function open_10255() {
     # The read-only port for the Kubelet to serve on with no authentication/authorization (set to 0 to disable)
     
     # USAGE
+    # curl http://localhost:10255/healthz
     # curl http://localhost:10255/metrics/
     # curl http://localhost:10255/metrics/cadvisor
     # curl http://localhost:10255/spec/
@@ -22,12 +23,25 @@ function open_10255() {
     # curl http://localhost:10255/stats/
     # curl http://localhost:10255/stats/summary
     #
-    # ABOUT metrics/cadvisor
+    # ABOUT spec/ pods/
+    # * http://cizixs.com/2016/10/25/kubernetes-intro-kubelet
+    #
+    # ABOUT healthz, metrics/, metrics/cadvisor, stats/
+    # * https://medium.com/@DazWilkin/kubernetes-metrics-ba69d439fac4
+    #
+    # ABOUT metrics/
+    # * https://hk.saowen.com/a/5b2b445e59abe32bac58344cf40d252109cc66cd81657d9929ea990e92ee2586
+    # * https://blog.freshtracks.io/a-deep-dive-into-kubernetes-metrics-part-3-container-resource-metrics-361c5ee46e66
+    #
+    # ABOUT metric 'container_memory_usage_bytes'
+    #
+    # curl http://localhost:10255/metrics/cadvisor | grep container_memory_usage_bytes
+    #
     # * https://prometheus.io/docs/guides/cadvisor/
     # * https://medium.com/@DazWilkin/kubernetes-metrics-ba69d439fac4
     # * https://hk.saowen.com/a/5b2b445e59abe32bac58344cf40d252109cc66cd81657d9929ea990e92ee2586
     # * https://blog.freshtracks.io/a-deep-dive-into-kubernetes-metrics-part-3-container-resource-metrics-361c5ee46e66
-    # * curl http://localhost:10255/metrics/cadvisor | grep container_memory_usage_bytes
+    #
 
     systemctl daemon-reload
     systemctl restart kubelet
