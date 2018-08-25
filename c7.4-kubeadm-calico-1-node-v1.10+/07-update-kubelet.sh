6b70b8bc4770ebc85ec634dd930cdaa05113aa29
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2018-08-25 11:23:16 +0800

    Update 07-update-kubelet.sh

--- c7.4-kubeadm-calico-1-node-v1.10+/07-update-kubelet.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/07-update-kubelet.sh
@@ -44,8 +44,9 @@ function open_10255() {
     # curl http://localhost:10255/metrics/cadvisor | grep container_memory_usage_bytes
     # prometheus query language
     # * container_memory_usage_bytes{job="cadvisor",namespace="add-on"}
-    #   * only list container :: container_memory_usage_bytes{job="cadvisor",namespace="add-on",container_name=~".+"}
-    #   * only list pod :: container_memory_usage_bytes{job="cadvisor",namespace="add-on",container_name!~".+"}
+    #   * only list namespace :: sum(container_memory_usage_bytes{job="cadvisor",namespace=~".+",container_name!~".+"}) by (namespace)  
+    #   * only list pod in a given ns :: container_memory_usage_bytes{job="cadvisor",namespace="add-on",container_name!~".+"}
+    #   * only list container in a given ns :: container_memory_usage_bytes{job="cadvisor",namespace="add-on",container_name=~".+"}
     #
     # * https://prometheus.io/docs/guides/cadvisor/
     # * https://blog.outlyer.com/top-kubernetes-metrics-to-monitor
