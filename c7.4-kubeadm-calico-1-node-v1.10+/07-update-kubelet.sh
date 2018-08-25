e314bb38137a719543573dd92ae4df1ea9e97d53
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2018-08-25 08:29:10 +0800

    Update 07-update-kubelet.sh

--- c7.4-kubeadm-calico-1-node-v1.10+/07-update-kubelet.sh
+++ c7.4-kubeadm-calico-1-node-v1.10+/07-update-kubelet.sh
@@ -20,6 +20,7 @@ function open_10255() {
     #
     # ABOUT metrics/cadvisor
     # * https://prometheus.io/docs/guides/cadvisor/
+    # * https://medium.com/@DazWilkin/kubernetes-metrics-ba69d439fac4
     # * https://hk.saowen.com/a/5b2b445e59abe32bac58344cf40d252109cc66cd81657d9929ea990e92ee2586
     # * https://blog.freshtracks.io/a-deep-dive-into-kubernetes-metrics-part-3-container-resource-metrics-361c5ee46e66
     # * curl http://localhost:10255/metrics/cadvisor | grep container_memory_usage_bytes
